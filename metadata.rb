9a071b9e8565d4b2c00ca9b67596279ad05c02e1
Kirt Martin <kirt.martin@centroid.com>
2017-12-11 13:49:18 -0500

    Initial creation of OEL

--- metadata.rb
+++ metadata.rb
@@ -1,12 +1,13 @@
 name 'centroid_chef_client'
-maintainer 'The Authors'
-maintainer_email 'you@example.com'
+maintainer 'Kirt Martin'
+maintainer_email 'kirt.martin@centroid.com'
 license 'All Rights Reserved'
 description 'Installs/Configures centroid_chef_client'
 long_description 'Installs/Configures centroid_chef_client'
-version '0.1.0'
+version '0.1.2'
 chef_version '>= 12.1' if respond_to?(:chef_version)
 
+depends 'chef-client'
 # The `issues_url` points to the location where issues for this cookbook are
 # tracked.  A `View Issues` link will be displayed on this cookbook's page when
 # uploaded to a Supermarket.
